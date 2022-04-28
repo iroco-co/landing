738e4ebfd10b8bf9c59159d69f72f05b6ac95f06
Bruno Thomas <bruno@barreverte.fr>
2022-04-28 11:39:48 +0200

    test: add precommit hook for linter and lint-staged

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	plugins: ['svelte3', '@typescript-eslint'],
+	ignorePatterns: ['*.cjs', 'src/*.js', 'semantic', 'test'],
+	overrides: [
+		{ files: ['*.svelte'],
+			processor: 'svelte3/svelte3',
+			rules : {
+				indent: "off"
+			}
+		},
+		{ files: ['test/**/*.test.js'],
+			plugins: ["jest"],
+			env: {
+				jest: true,
+				"jest/globals": true
+			},
+			rules: {
+				"jest/no-disabled-tests": "warn",
+				"jest/no-focused-tests": "error",
+				"jest/no-identical-title": "error",
+				"jest/prefer-to-have-length": "warn",
+				"jest/valid-expect": "error"
+			}
+		}
+	],
+	settings: {
+		'svelte3/typescript': () => require('typescript'),
+		'svelte3/ignore-styles': () => true
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2019
+	},
+	rules: {
+		indent: ['error', 2]
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	}
+};
