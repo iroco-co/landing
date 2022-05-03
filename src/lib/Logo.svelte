83be30e977d6321c44c597e59425ff8bbd1667fd
Joanna <diasjoanna85@gmail.com>
2022-05-03 11:36:15 +0200

    feat: add Logo component

---
+++ src/lib/Logo.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let size = 48;
+  export let margin = 2;
+</script>
+
+<div class="logo">
+  <img
+    class="logo__image"
+    style="width: {size}px;margin-bottom: {margin}em"
+    src="/iroco.svg"
+    alt="Iroco logo"
+  />
+  <h2 class="logo__title">iroco</h2>
+</div>
+
+<style lang="scss">
+  .logo {
+    display: flex;
+    &__title {
+      margin-left: 1em;
+    }
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n';
+  import Logo from '../lib/Logo.svelte';
 </script>
 
 <svelte:head>
   <title>{$_('page.title')}</title>
 </svelte:head>
 
+<main class="homepage">
+  <nav class="homepage__header">
+    <Logo />
+  </nav>
+</main>
 <h1>{$_('home.title')}</h1>
