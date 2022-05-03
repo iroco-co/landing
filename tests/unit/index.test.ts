47afafb685dd43439cf0d69372663973cf33ab0b
Joanna <diasjoanna85@gmail.com>
2022-05-03 10:34:55 +0200

    ci: fix test

--- tests/unit/index.test.ts
+++ tests/unit/index.test.ts
@@ -1,12 +1,17 @@
 /**
  * @jest-environment jsdom
  */
-import index from '../../src/routes/index.svelte';
-import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/svelte';
+import index from '../../src/routes/index.svelte'
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/svelte'
+import {addMessages, init} from 'svelte-i18n'
+import fr from '../../src/i18n/fr.json'
+
+addMessages('fr', fr)
+init({fallbackLocale: 'fr', initialLocale: 'fr'})
 
 test('Welcome', () => {
   const { getByText } = render(index);
 
-  expect(getByText('Welcome to SvelteKit')).toBeInTheDocument();
+  expect(getByText('Bienvenu à Iroco !')).toBeInTheDocument();
 });
