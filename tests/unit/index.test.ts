b7d7a8ba75aa5954ce98a2ae91ee602d0af77f89
Joanna <diasjoanna85@gmail.com>
2022-05-03 10:54:02 +0200

    ci: fix format on test

--- tests/unit/index.test.ts
+++ tests/unit/index.test.ts
@@ -1,14 +1,14 @@
 /**
  * @jest-environment jsdom
  */
-import index from '../../src/routes/index.svelte'
-import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/svelte'
-import {addMessages, init} from 'svelte-i18n'
-import fr from '../../src/i18n/fr.json'
+import index from '../../src/routes/index.svelte';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/svelte';
+import { addMessages, init } from 'svelte-i18n';
+import fr from '../../src/i18n/fr.json';
 
-addMessages('fr', fr)
-init({fallbackLocale: 'fr', initialLocale: 'fr'})
+addMessages('fr', fr);
+init({ fallbackLocale: 'fr', initialLocale: 'fr' });
 
 test('Welcome', () => {
   const { getByText } = render(index);
