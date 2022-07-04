1efada4607df310c805ecf600898f8756d36f3a6
Joanna <diasjoanna85@gmail.com>
2022-07-04 13:06:35 +0200

    feat: adds button aspect to last item in Header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,7 +3,7 @@
   import { Navigation } from '@iroco/ui';
   const navigationItems = [
     {
-      hrefOrCallback: '/about',
+      hrefOrCallback: '/',
       name: $_('header.about')
     },
     {
@@ -31,7 +31,19 @@
 
 <style lang="scss">
   @use 'node_modules/@iroco/ui/lib/colors';
-
+  @use 'node_modules/@iroco/ui/lib/constants';
+  :global(.nav__topbar__item-container) {
+    align-items: center;
+  }
+  :global(.nav__topbar__item:last-child) {
+    padding: 1em 2em;
+    background: colors.$green;
+    border: none;
+    border-radius: constants.$border-radius;
+    &:hover {
+      box-shadow: inset 0 0 0 10em rgba(0, 0, 0, 0.2);
+    }
+  }
   :global(.navigation) {
     position: fixed;
     background-color: colors.$darkBlue;
