cdc54ed86a1dbb42b289d9a47d41f3224fb04449
Joanna <diasjoanna85@gmail.com>
2022-08-11 11:09:42 +0200

    refactor: add IrocologoSignupbutton component iroco-co/landing#2

---
+++ src/lib/IrocologoSignupbutton.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { _ } from 'svelte-i18n';
+  import { Icon, IrocoLogo } from '@iroco/ui/';
+</script>
+
+<div class="irocologo-signupbutton">
+  <div class="irocologo-signupbutton__logo">
+    <IrocoLogo width="10em" height="10em" />
+  </div>
+  <div class="irocologo-signupbutton__btn">
+    <a
+      class={`iroco-ui-button iroco-ui-button--regular iroco-ui-button--success`}
+      href="https://app.iroco.co/signup/"
+    >
+      <Icon name="chevron-right" color="colors.$nightBlue" />
+      {$_('footer.subscribe')}
+    </a>
+  </div>
+</div>
+
+<style lang="scss">
+  @import 'node_modules/@iroco/ui/lib/button';
+  .irocologo-signupbutton {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 2em 4em;
+    margin-bottom: 5em;
+  }
+</style>
