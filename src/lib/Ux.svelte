9c31f52c3386fa709f061aee092b362a2f4d4c4f
Joanna <diasjoanna85@gmail.com>
2022-08-17 16:48:16 +0200

    feat: add Ux component iroco-co/landing#5

---
+++ src/lib/Ux.svelte
@@ -0,0 +1,25 @@
+<span class="ux">
+  <title>User Experience</title>
+  <svg
+    width="103"
+    height="202"
+    viewBox="0 0 103 202"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_4021_44967)">
+      <path
+        d="M81.83 1.5H20.31C9.92152 1.5 1.5 9.92152 1.5 20.31V180.97C1.5 191.358 9.92152 199.78 20.31 199.78H81.83C92.2185 199.78 100.64 191.358 100.64 180.97V20.31C100.64 9.92152 92.2185 1.5 81.83 1.5Z"
+        fill="#211D28"
+        stroke="#FFE032"
+        stroke-width="3"
+        stroke-miterlimit="10"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_4021_44967">
+        <rect width="102.14" height="201.28" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+</span>
