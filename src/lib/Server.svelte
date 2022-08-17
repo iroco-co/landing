1e6858672f1678966c1868256b551f68bc29f1e6
Joanna <diasjoanna85@gmail.com>
2022-08-17 15:25:10 +0200

    feat: add Server component iroco-co/landing#5

---
+++ src/lib/Server.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  export let size: number;
+</script>
+
+<span class="navigation-landing-assets-server small">
+  <svg viewBox="0 0 145.25 167.72" height={size}>
+    <title>Server</title>
+    <line
+      x1="72.29"
+      y1="83.86"
+      x2="143.75"
+      y2="42.8"
+      fill="none"
+      stroke="#fff"
+      stroke-miterlimit={10}
+      stroke-width={3}
+    />
+    <line
+      x1="1.17"
+      y1="42.8"
+      x2="72.29"
+      y2="83.86"
+      fill="none"
+      stroke="#00b9ff"
+      stroke-miterlimit={10}
+      stroke-width={3}
+    />
+    <line
+      x1="72.29"
+      y1="83.86"
+      x2="72.29"
+      y2="165.99"
+      fill="none"
+      stroke="#ff504d"
+      stroke-miterlimit={10}
+      stroke-width={3}
+    />
+    <polygon
+      points="72.63 1.73 1.5 42.8 1.5 124.93 72.63 165.99 143.75 124.93 143.75 42.8 72.63 1.73"
+      fill="none"
+      stroke="#00d692"
+      stroke-miterlimit={10}
+      stroke-width={3}
+    />
+  </svg>
+</span>
