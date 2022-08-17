d0b0a42721af211b7e83bd4666383f20b5639e7a
Joanna <diasjoanna85@gmail.com>
2022-08-17 17:56:58 +0200

    feat: create Mail component iroco-co/landing#5

---
+++ src/lib/Mail.svelte
@@ -0,0 +1,26 @@
+<span>
+  <svg class="signup__offer__card__svglistgroup__icon" viewBox="0 0 58.96 85.92" height="165">
+    <title>Mail</title>
+    <rect
+      x="1.95"
+      y="28.91"
+      width="55.06"
+      height="55.06"
+      fill="none"
+      stroke="#00d692"
+      stroke-linejoin="bevel"
+      stroke-width={1}
+    />
+    <rect
+      x="10.01"
+      y="9.44"
+      width="38.94"
+      height="38.94"
+      transform="translate(-11.81 29.31) rotate(-45)"
+      fill="none"
+      stroke="#fff"
+      stroke-linejoin="bevel"
+      stroke-width={1}
+    />
+  </svg>
+</span>
