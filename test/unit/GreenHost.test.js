39b863290bc912691aaaec53c26a425e2c058d6a
Joanna <diasjoanna85@gmail.com>
2022-08-30 16:18:23 +0200

    test: add rendering test to Green Host iroco-co/landing#5

---
+++ test/unit/GreenHost.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/svelte';
+import GreenHost from '../../src/lib/GreenHost.svelte';
+import en from '../../src/i18n/en.json';
+import { addMessages, init } from 'svelte-i18n';
+
+addMessages('en', en);
+init({ fallbackLocale: 'en', initialLocale: 'en' });
+
+describe('Testing GreenHost Component', () => {
+  test('its content is correctly rendered', () => {
+    const { getByText } = render(GreenHost, { title: 'A Title', size: '220' });
+    expect(getByText('A Title')).toBeInTheDocument();
+  });
+});
