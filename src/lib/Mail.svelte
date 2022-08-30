e6e8a1b699cf2e1d7e10aeebe7d74aceba04c9f3
Joanna <diasjoanna85@gmail.com>
2022-08-30 17:38:02 +0200

    refactor: add conditional ternary operator to the title of Mail iroco-co/landing#5 test: add rendering test

--- src/lib/Mail.svelte
+++ src/lib/Mail.svelte
@@ -5,58 +5,45 @@
 </script>
 
 <span class="mail">
-  {#if title == null}
-    <svg class="mail__svg" viewBox="0 0 58.96 85.92" height={size}>
-      <title>Mail</title>
-      <rect
-        x="1.95"
-        y="28.91"
-        width="55.06"
-        height="55.06"
-        fill="none"
-        stroke="#00d692"
-        stroke-linejoin="bevel"
-        stroke-width={strokeWidth}
-      />
-      <rect
-        x="10.01"
-        y="9.44"
-        width="38.94"
-        height="38.94"
-        transform="translate(-11.81 29.31) rotate(-45)"
-        fill="none"
-        stroke="#fff"
-        stroke-linejoin="bevel"
-        stroke-width={strokeWidth}
-      />
-    </svg>
-  {:else}
-    <div class="mail__svg-title">
-      <svg viewBox="0 0 58.96 85.92" height={size}>
-        <title>Mail</title>
-        <rect
-          x="1.95"
-          y="28.91"
-          width="55.06"
-          height="55.06"
-          fill="none"
-          stroke="#00d692"
-          stroke-linejoin="bevel"
-          stroke-width={strokeWidth}
-        />
-        <rect
-          x="10.01"
-          y="9.44"
-          width="38.94"
-          height="38.94"
-          transform="translate(-11.81 29.31) rotate(-45)"
-          fill="none"
-          stroke="#fff"
-          stroke-linejoin="bevel"
-          stroke-width={strokeWidth}
-        />
-      </svg>
-      <h4>{title}</h4>
-    </div>
-  {/if}
+  <svg viewBox="0 0 58.96 85.92" height={size}>
+    <title>Mail</title>
+    <rect
+      x="1.95"
+      y="28.91"
+      width="55.06"
+      height="55.06"
+      fill="none"
+      stroke="#00d692"
+      stroke-linejoin="bevel"
+      stroke-width={strokeWidth}
+    />
+    <rect
+      x="10.01"
+      y="9.44"
+      width="38.94"
+      height="38.94"
+      transform="translate(-11.81 29.31) rotate(-45)"
+      fill="none"
+      stroke="#fff"
+      stroke-linejoin="bevel"
+      stroke-width={strokeWidth}
+    />
+  </svg>
+  <h4 class:hidden={title == null ? 'hidden' : ''}>{title}</h4>
 </span>
+
+<style lang="scss">
+  .mail {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-content: center;
+
+    h4 {
+      transform: translateY(-1.6em);
+    }
+  }
+  .hidden {
+    display: none;
+  }
+</style>
