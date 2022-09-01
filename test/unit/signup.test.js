1fd6178cc01b1a80bc0d36d1b066dc09868eb3b4
Joanna <diasjoanna85@gmail.com>
2022-09-01 16:31:12 +0200

    feat: remove faq from signup iroco-co/landing#16

--- test/unit/signup.test.js
+++ test/unit/signup.test.js
@@ -3,7 +3,7 @@
  */
 import signup from '../../src/routes/signup.svelte';
 import '@testing-library/jest-dom/extend-expect';
-import { screen, render } from '@testing-library/svelte';
+import { render } from '@testing-library/svelte';
 import { addMessages, init } from 'svelte-i18n';
 import en from '../../src/i18n/en.json';
 
@@ -12,31 +12,12 @@ init({ fallbackLocale: 'en', initialLocale: 'en' });
 
 describe('Testing Signup Page', () => {
   test('headers are correctly rendered', () => {
-    const { getByText } = render(signup, { opened: false });
+    const { getByText } = render(signup);
 
     expect(getByText('Our offer')).toBeInTheDocument();
     expect(getByText("If it's free, it's the planet the product")).toBeInTheDocument();
     expect(getByText('Ethics and confidentiality')).toBeInTheDocument();
     expect(getByText("We don't change email every year")).toBeInTheDocument();
     expect(getByText('Roadmap')).toBeInTheDocument();
-    expect(getByText('FAQ')).toBeInTheDocument();
-  });
-
-  test('when opened equals to false, readmore button is displayed without class "opened"', () => {
-    const { container } = render(signup, { opened: false });
-    const button = screen.getByText('Read more');
-    const faqcontainer = container.querySelector('.signup__faq__container__q-a');
-
-    expect(button).toBeInTheDocument();
-    expect(faqcontainer).not.toHaveClass('opened');
-  });
-
-  test('when opened equals to true, the class "opened" is applied and the content of the readbutton changes', () => {
-    const { container } = render(signup, { opened: true });
-    const button = screen.getByText('Read less');
-    const faqcontainer = container.querySelector('.signup__faq__container__q-a');
-
-    expect(button).toBeInTheDocument();
-    expect(faqcontainer).toHaveClass('opened');
   });
 });
