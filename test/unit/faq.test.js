853bf929d80e03ae229ee35ad7c1af427960d405
Joanna <diasjoanna85@gmail.com>
2022-09-05 09:02:16 +0200

    ci: fix remove lorem ipsum from faq test iroco-co/landing#16

--- test/unit/faq.test.js
+++ test/unit/faq.test.js
@@ -15,11 +15,6 @@ describe('Testing Faq Page', () => {
     const { getByText } = render(faq);
 
     expect(getByText('FAQ')).toBeInTheDocument();
-    expect(
-      getByText(
-        'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.'
-      )
-    ).toBeInTheDocument();
     expect(getByText('What exactly does your offer include?')).toBeInTheDocument();
   });
 });
