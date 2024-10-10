4a5e2542b46ec0fde13fe8884d3d08e6fa59f75e
Bruno Thomas <bruno@barreverte.fr>
2024-10-10 14:39:10 +0200

    fix: url for matomo

--- static/tracking.js
+++ static/tracking.js
@@ -1,5 +1,5 @@
 var idSite = 1;
-var matomoTrackingApiUrl = 'https://matomo.iroco.co/matomo.php';
+var matomoTrackingApiUrl = 'https://matomo.iroco.co:8080/matomo.php';
 
 var _paq = window._paq = window._paq || [];
 _paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
