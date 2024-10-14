23c1e5c6977ee67d1e0b702b84b327d2982500a0
Arthur Billebaut <abillebaut@iroco.fr>
2024-10-14 09:39:45 +0000

    fix: receve matomo statistics iroco-co/iroco-app-ansible#51

--- static/tracking.js
+++ static/tracking.js
@@ -1,8 +1,8 @@
 var idSite = 1;
-var matomoTrackingApiUrl = 'https://matomo.iroco.co:8080/matomo.php';
+var matomoTrackingApiUrl = "https://matomo.iroco.co:8080/matomo.php";
 
-var _paq = window._paq = window._paq || [];
-_paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
-_paq.push(['setSiteId', idSite]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
+var _paq = (window._paq = window._paq || []);
+_paq.push(["setTrackerUrl", matomoTrackingApiUrl]);
+_paq.push(["setSiteId", idSite]);
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
